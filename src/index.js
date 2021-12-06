61b1e9b9539fc648886482e4394575672751307e
rdeepak2002 <rdeepak2002@gmail.com>
2021-12-06 14:08:17 -0500

    fixed server

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-const io = require("socket.io")(8900, {
-  cors: {
-    origin: "http://localhost:3000",
-  },
-});
+// const io = require("socket.io")(8900, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//   },
+// });
 let users = [];
 
 const addUser = (userId, socketId) => {
@@ -30,32 +30,32 @@ const getUser = (userId) => {
   return users.find((user) => user.userId === userId);
 };
 
-io.on("connection", (socket) => {
-  //when ceonnect
-  console.log("a user connected.");
-
-  //take userId and socketId from user
-  socket.on("addUser", (userId) => {
-    addUser(userId, socket.id);
-    io.emit("getUsers", users);
-  });
-
-  //send and get message
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId);
-    io.to(user.socketId).emit("getMessage", {
-      senderId,
-      text,
-    });
-  });
-
-  //when disconnect
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-    io.emit("getUsers", users);
-  });
-});
+// io.on("connection", (socket) => {
+//   //when ceonnect
+//   console.log("a user connected.");
+//
+//   //take userId and socketId from user
+//   socket.on("addUser", (userId) => {
+//     addUser(userId, socket.id);
+//     io.emit("getUsers", users);
+//   });
+//
+//   //send and get message
+//   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+//     const user = getUser(receiverId);
+//     io.to(user.socketId).emit("getMessage", {
+//       senderId,
+//       text,
+//     });
+//   });
+//
+//   //when disconnect
+//   socket.on("disconnect", () => {
+//     console.log("a user disconnected!");
+//     removeUser(socket.id);
+//     io.emit("getUsers", users);
+//   });
+// });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
